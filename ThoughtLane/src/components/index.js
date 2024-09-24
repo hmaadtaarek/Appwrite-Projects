6df158d599667c6e56a1a4662d56d82cf62d5c0b
hmaadtarek <hammadjutthj6355@gmail.com>
2024-09-24 18:55:19 +0500

    componts under development

---
+++ ThoughtLane/src/components/index.js
@@ -0,0 +1,11 @@
+import AuthLayout from './AuthLayout'
+import Button from './Button'
+import Container from './Container'
+import Footer from './Footer'
+import Header from './Header'
+import Input from './Input'
+import Login from './Login'
+import Logout from './Logout'
+import Signup from './Signup'
+
+export { AuthLayout, Button, Container, Footer, Header, Input, Login, Logout, Signup }
